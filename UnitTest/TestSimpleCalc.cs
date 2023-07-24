7b3f9790929aefb0f0d6d763b5aff239d3440e0f
ShanKonduru <ShanKonduru@gmail.com>
2023-07-24 21:59:18 +0530

    added subtract method

--- UnitTest/TestSimpleCalc.cs
+++ UnitTest/TestSimpleCalc.cs
@@ -6,9 +6,16 @@ namespace shan.cmdlets.UnitTest;
 public class TestSimpleCalc
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestSimpleCalcAddMethod()
     {
         int result = SimpleCalc.Add(10, 20);
         Assert.AreEqual(result, 30, "Add did not work.");
+    }
+
+    [TestMethod]
+    public void TestSimpleCalcSubMethod()
+    {
+        int result = SimpleCalc.Subtract(20, 10);
+        Assert.AreEqual(result, 10, "Subtract did not work.");
     }
 }
--- class/SimpleCalc.cs
+++ class/SimpleCalc.cs
@@ -2,7 +2,13 @@
 
 public class SimpleCalc
 {
-    public static int Add(int a, int b){
-        return a+b;
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
     }
 }
